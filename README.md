9ffe0d2b22a0809db4202e5d965b56dfe06922df
Ruben Torres Bonet <rubentorresbonet@gmail.com>
2022-08-16 11:18:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+https://levelup.gitconnected.com/private-npm-packages-in-github-package-registry-fbfda43acab3
+
+1. Change something
+2. Increase version in package.json
+3. git commit
+4. npm publish
+
+...
+
+npm update everywhere
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+const { chromium } = require('playwright-core');
+
+export default class Encharge
+{
+  private browser : any
+  private page : any
+
+  constructor(){}
+
+  public async start(headless: boolean = true)
+  {
+    this.browser = await chromium.launch({headless: headless});
+    this.page = await this.browser.newPage();
+  }
+
+  public async finish()
+  {
+    await this.browser.close();
+  }
+
+  public async login(user: string, pwd: string)
+  {
+    await this.page.goto('https://app.encharge.io/login');
+    await this.page.fill('[placeholder="Email"]', user);
+    await this.page.fill('[placeholder="Password"]', pwd);
+    await Promise.all([
+      this.page.waitForNavigation(/*{ url: 'https://app.encharge.io/emails?emails-folder-item=allEmails' }*/),
+      this.page.click('button:has-text("Login")')
+    ]);
+  }
+
+  public async createEmail(name: string, subject: string, content: string)
+  {
+    await this.page.goto('https://app.encharge.io/emails?emails-folder-item=allEmails');
+    await this.page.waitForTimeout(500);
+  
+    //console.log('Creating email...')
+    await this.page.click('[data-testid="createNewButton"]');
+    await this.page.waitForTimeout(500);
+    await this.page.click('[data-testid="addTextEmail"]');
+    await this.page.waitForTimeout(500);
+
+    //console.log('Preparing email...')
+    await this.page.click('text=Send something awesome...');
+    await this.page.fill('text=Send something awesome...', content);
+    await this.page.waitForTimeout(500);
+
+    // Email name
+    await this.page.click('[placeholder="Enter email name"]');
+    await this.page.fill('[placeholder="Enter email name"]', name);
+    await this.page.waitForTimeout(500);
+
+    // Subject
+    await this.page.click('[value="No Subject"]');
+    await this.page.fill('[value="No Subject"]', subject);
+    await this.page.waitForTimeout(500);
+  
+    // Click text=Save
+    //console.log('Saving email...')
+    await Promise.all([
+      this.page.waitForNavigation(/*{ url: 'https://app.encharge.io/emails?emails-folder-item=allEmails&email=75738' }*/),
+      this.page.click('text=Save and close')
+    ]);
+    await this.page.waitForTimeout(3000);
+  }
+
+};
